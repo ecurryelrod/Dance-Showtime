05f557fac0f4eb745248a0f0fed619be5f4ed302
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-11-18 17:04:01 -0700

    removed performance_categories migration from schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_18_235804) do
+ActiveRecord::Schema.define(version: 2021_11_18_233846) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "performance_categories", force: :cascade do |t|
-    t.integer "performance_id", null: false
-    t.integer "category_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["category_id"], name: "index_performance_categories_on_category_id"
-    t.index ["performance_id"], name: "index_performance_categories_on_performance_id"
   end
 
   create_table "performances", force: :cascade do |t|
@@ -62,8 +53,6 @@ ActiveRecord::Schema.define(version: 2021_11_18_235804) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "performance_categories", "categories"
-  add_foreign_key "performance_categories", "performances"
   add_foreign_key "performances", "users"
   add_foreign_key "performances", "venues"
 end
