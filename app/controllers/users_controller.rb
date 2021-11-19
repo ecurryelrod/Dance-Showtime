ed8bfad0d1d3471fb577cd39a7ee57e310270904
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-11-19 16:06:21 -0700

    updated users controller create and show methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,23 +4,23 @@ class UsersController < ApplicationController
     end
 
     def create
-        @user = User.new(user_params)
-        if @user.save
-            session[:user_id] = @user.id
-            redirect_to user_path(@user)
+        user = User.new(user_params)
+        if user.save
+            session[:user_id] = user.id
+            redirect_to user
         else
-            flash[:error] = "Must fill in all required fields."
-            render :new
+            flash[:error] = user.errors.full_messages
+            redirect_to '/signup'
         end
     end
 
     def show
-        
+        @user = User.find(params[:id])
     end
 
     private
 
     def user_params
-        params.require(:user).permit(:name, :email, :password)
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
