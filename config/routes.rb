c0a3871c4285efa8f079714552589d6ba6ca6436
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-11-19 16:14:26 -0700

    updates routes and user show page

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # resources :categories
   # resources :venues
   # resources :performances
-  resources :users, only: [:new, :create]
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # Almost every application defines a route for the root path ("/") at the top of this file.
