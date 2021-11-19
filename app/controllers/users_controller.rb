d585416b271a283386f4db32aa7b062449f5367c
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-11-19 14:46:18 -0700

    User signup - added code in users_controller, views/users/new, and routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            flash[:error] = "Must fill in all required fields."
+            render :new
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  resources :categories
-  resources :venues
-  resources :performances
-  resources :users
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+
+  # resources :categories
+  # resources :venues
+  # resources :performances
+  resources :users, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # Almost every application defines a route for the root path ("/") at the top of this file.
