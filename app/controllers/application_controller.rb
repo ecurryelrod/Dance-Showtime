03b275df35e84a5b1f900db3aa32d75f85e070e1
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-11-20 16:42:40 -0700

    added current_user, login_required, before_action to App Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+    before_action :login_required
+    helper_method :current_user
+
+    def current_user
+        @user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+
+    def logged_in
+        !!current_user
+    end
+
+    def login_required
+        unless logged_in
+            flash[:error] = "Please log in to view your profile."
+            redirect_to '/'
+        end
+    end
 end
