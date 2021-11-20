91896751a69912252a24f5386829a0db3fe91293
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-11-20 16:46:27 -0700

    added statid controller, home method and view, root route, and added logout button to user show page

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,5 @@
+class StaticController < ApplicationController
+    skip_before_action :login_required, only: [:home]
+
+    def home; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
+  root 'static#home'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  # post '/logout', to: 'sessions#destroy'
+  post '/logout', to: 'sessions#destroy'
 
   # resources :categories
   # resources :venues
   # resources :performances
   resources :users, only: [:show]
+  resources :sessions, only: [:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # Almost every application defines a route for the root path ("/") at the top of this file.
