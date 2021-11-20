ef2b2d107793c88f2ab20228f962b9e235bfe3f4
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-11-19 17:46:55 -0700

    added login routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  # post '/logout', to: 'sessions#destroy'
 
   # resources :categories
   # resources :venues
