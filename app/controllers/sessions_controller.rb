c3bd9ca7d5c3c614926463923006255992e77e69
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-11-20 19:16:38 -0700

    added notes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,14 @@ class SessionsController < ApplicationController
     def new; end
 
     def create
-        user = User.find_by(email: params[:email])
-        if user.try(:authenticate, params[:password])
+        @user = User.find_by(email: params[:email])
+        if @user.try(:authenticate, params[:password])
         # if user && user.authenticate(params[:password])
-            session[:user_id] = user.id
-            redirect_to user
+            session[:user_id] = @user.id
+            redirect_to @user
         else
+            # binding.pry
+            # flash[:error] = user.errors.full_messages # cannot do this as user is nil if email and password field blank. Not sure how to get it to work if even possible. Tried googling and didn't find anything useful... 
             flash[:error] = "Incorrect email/password. Try again or create an account."
             redirect_to login_path
         end
