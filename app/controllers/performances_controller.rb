ecec5554eeb49eaa2e7f7c437106dd55e42e0af6
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-11-21 11:59:20 -0700

    added CRUD actions to performances controller, created views/performances/new page, added routes resources for performances#new and show

--- app/controllers/performances_controller.rb
+++ app/controllers/performances_controller.rb
@@ -1,2 +1,30 @@
 class PerformancesController < ApplicationController
+    # skip_before_action :login_required, only: [:new, :show]
+
+    def new
+        @performance = Performance.new
+    end
+
+    def create
+        # performance = Performance.create(performance_params)
+        performance = current_user.performances.build(performance_params)
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+    private 
+
+    def performance_params
+        params.require(:performance).permit(:title, :date_time, :ticket_price, :ticket_url, :description, :company_url, :user_id, :venue_id)
+    end
 end
