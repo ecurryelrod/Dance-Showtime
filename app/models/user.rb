56d365739d3d3a3555355c45f1fd0a361fc0b618
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-11-20 19:26:37 -0700

    updated validations in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
     has_secure_password
-    validates :name, :email, presence: true 
-    validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-    
+    # validates :name, :email, presence: true
+    validates_presence_of :name, :email
+    # validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates_uniqueness_of :email
+
     has_many :performances
     has_many :venues, through: :performances
 end
