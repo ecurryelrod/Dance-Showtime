71c95869262c1a110cd9dcf6df4a5a62c289bed4
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-11-21 17:57:42 -0700

    updated performances controller create and show methods

--- app/controllers/performances_controller.rb
+++ app/controllers/performances_controller.rb
@@ -1,13 +1,26 @@
 class PerformancesController < ApplicationController
-    # skip_before_action :login_required, only: [:new, :show]
+    skip_before_action :login_required, only: [:show]
 
     def new
         @performance = Performance.new
     end
 
     def create
-        # performance = Performance.create(performance_params)
-        performance = current_user.performances.build(performance_params)
+        # binding.pry
+        # @performance = Performance.create(performance_params)
+        @performance = current_user.performances.build(performance_params)
+        # @performance.build_venue
+        if @performance.save
+            redirect_to performance_path(@performance)
+        else
+            flash[:error] = @performance.errors.full_messages
+            render :new
+        end
+    end
+
+    def show
+        # binding.pry
+        @performance = Performance.find(params[:id])
     end
 
     def edit
@@ -25,6 +38,6 @@ class PerformancesController < ApplicationController
     private 
 
     def performance_params
-        params.require(:performance).permit(:title, :start_date, :end_date, :time, :ticket_price, :ticket_url, :description, :company_url, :user_id, :venue_id)
+        params.require(:performance).permit(:title, :start_date, :end_date, :time, :ticket_price, :ticket_url, :description, :company_url, :user_id, :venue_id, venue_attributes:[:id, :name, :address_1, :adress_2, :city, :state, :zipcode, :venue_url])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   # resources :venues
   # resources :performances
   resources :users, only: [:show]
-  resources :performances, only: [:new, :create]
+  resources :performances, only: [:new, :create, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # Almost every application defines a route for the root path ("/") at the top of this file.
