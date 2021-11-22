16b4943e0edd134670825ef84d78400ee8719f9f
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-11-21 18:02:27 -0700

    updated performance model to accept nested attributes, completed views/performance/new form and created performance show page

--- app/models/performance.rb
+++ app/models/performance.rb
@@ -1,8 +1,13 @@
 class Performance < ApplicationRecord
   belongs_to :user
   belongs_to :venue
+  accepts_nested_attributes_for :venue
 
   # not sure this association working properly. check later
   has_many :performance_categories
   has_many :categories, through: :performance_categories
+
+  # def venue_attributes=(attribute)
+  #   self.venue = Venue.find_or_create_by(name: attribute[:name]) if !attribute[:name].blank?
+  # end
 end
