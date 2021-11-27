be1ee8341a97a3b4dd2d04fbeee36d2222a70421
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-11-27 12:02:22 -0700

    updated users#create and sessions#create to redirect to root path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,12 @@ class SessionsController < ApplicationController
 
     def create
         @user = User.find_by(email: params[:email])
-        if @user.try(:authenticate, params[:password])
         # if user && user.authenticate(params[:password])
+        if @user.try(:authenticate, params[:password])
             session[:user_id] = @user.id
-            redirect_to user_path(@user)
+            # redirect_to user_path(@user.name)
+            redirect_to root_path
         else
-            # binding.pry
             # flash[:error] = user.errors.full_messages # cannot do this as user is nil if email and password field blank. Not sure how to get it to work if even possible. Tried googling and didn't find anything useful... 
             flash[:error] = "Incorrect email/password. Try again or create an account."
             redirect_to login_path
