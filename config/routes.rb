695e3bfeec796dc7d8d1a129fe03840f243218f9
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-11-27 11:59:03 -0700

    removed commented out code from routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   post '/signup', to: 'users#create'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  # post 'logout', to: 'sessions#destroy'
   delete '/logout', to: 'sessions#destroy'
 
   # resources :categories
