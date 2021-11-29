54b0761d51d8a4359ee1c35921d4ad692ac02648
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-11-29 15:22:18 -0700

    updated logout route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   post '/signup', to: 'users#create'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy'
+  # delete '/logout', to: 'sessions#destroy'
 
   # resources :categories
   resources :venues, only: [:show]
