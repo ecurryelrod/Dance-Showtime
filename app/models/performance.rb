3946c936831cb129d1cf0378855cffa3e7aaa555
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-11-28 17:35:31 -0700

    updated performance model

--- app/models/performance.rb
+++ app/models/performance.rb
@@ -1,15 +1,15 @@
 class Performance < ApplicationRecord
   belongs_to :user
   belongs_to :venue
-  # accepts_nested_attributes_for :venue, reject_if: proc {|attr| attr[:name].blank?}
+  accepts_nested_attributes_for :venue, reject_if: proc {|attr| attr[:name].blank?}
 
-  validates_presence_of :title, :start_date, :time
+  validates_presence_of :title, :start_date, :time, :ticket_price, :description, :ticket_url, :company_url
 
   # not sure this association working properly. check later
   has_many :performance_categories
   has_many :categories, through: :performance_categories
 
-  def venue_attributes=(attribute)
-    self.venue = Venue.find_or_create_by(name: attribute[:name]) if !attribute[:name].blank?
-  end
+  # def venue_attributes=(attribute)
+  #   self.venue = Venue.find_or_create_by(name: attribute[:name]) if !attribute[:name].blank?
+  # end
 end
