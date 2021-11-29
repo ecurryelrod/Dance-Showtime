6eeda5d2339bb661f6e7822f0c950e20e9e5d0be
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-11-28 17:36:07 -0700

    added validations to venue model

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,4 +1,8 @@
 class Venue < ApplicationRecord
     has_many :performances
     has_many :users, through: :performances
+
+    validates_presence_of :name, :address_1, :city, :state, :zipcode, :venue_url
+    validates_uniqueness_of :name
+    validates_format_of :venue_url, :with => URI::regexp(%w(http https)) 
 end
