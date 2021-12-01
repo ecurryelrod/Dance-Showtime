06900f0ef1901bd6902f344bc09a8bee40a15c20
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-12-01 10:33:08 -0700

    updated routes, cleaned up code and deleted comments

--- config/routes.rb
+++ config/routes.rb
@@ -8,19 +8,19 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
 
   get '/logout', to: 'sessions#destroy'
-  # delete '/logout', to: 'sessions#destroy'
   
-  # get '/auth/google_oauth2/callback', to: 'sessions#google_login'
   get '/auth/google_oauth2/callback', to: 'sessions#create'
 
-
-  # resources :categories
   resources :venues, only: [:show]
-  # resources :performances
+
   resources :users, only: [:show] do
     resources :performances, only: [:new, :create, :show, :edit, :update]
   end
+
   resources :performances, only: [:new, :create, :show, :update]
+
+   # resources :categories
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # Almost every application defines a route for the root path ("/") at the top of this file.
