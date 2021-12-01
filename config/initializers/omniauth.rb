f6ca1d494b3524cc676c3a1feb54fdeac7bd95b7
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-12-01 10:36:46 -0700

    updated credentials.yml file and omniauth.rb file to read id and secret from credentials instead of .env

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,9 @@
-# Rails.application.config.middleware.use OmniAuth::Builder do
-#     provider :google_oauth2, Rails.application.credentials.google_oauth2[:app_id], Rails.application.credentials.google[:app_secret]
-# end
-
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
+    provider :google_oauth2, Rails.application.credentials.google_oauth2[:app_id], Rails.application.credentials.google_oauth2[:app_secret]
 end
+
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#     provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
+# end
 
 OmniAuth.config.allowed_request_methods = %i[get]
