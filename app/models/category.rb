83cf033a1e013a022a499cc40ff67df380658d8e
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-12-11 16:39:52 -0700

    added index action to categories controller and deleted commentes from category model - anything to do with categories and deleted comments in category model - anything with categories is a stretch goal but association has been tested and is working properly

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,4 @@
 class Category < ApplicationRecord
-    # not sure this association working properly. check later
     has_many :performance_categories
     has_many :performances, through: :performance_categories
 end
